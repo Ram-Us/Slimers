8f4f55c5ff035f142e25ef37ca540bb325ca24db
Rams-Us <0rg3261c0y2064j@au.com>
2025-10-03 23:17:54 +0900

    add:前と一緒

--- DigitalGame_Slime/Assets/Scripts/SlimeController.cs
+++ DigitalGame_Slime/Assets/Scripts/SlimeController.cs
@@ -9,10 +9,11 @@ public class SlimeController : MonoBehaviour
 {
 
     private float jumpPower = 3f;
-    private float sideForce = 1f;
+    //private float sideForce = 1f;
     private Rigidbody rb;
+    private Animator anim;
     private bool isGrounded=true;
-    private bool isSquatting = false; // しゃがみ状態フラグ
+    //private bool isSquatting = false; // しゃがみ状態フラグ
     private Vector3 PresentPosition;
     private Vector3 PresentScale;
     private Vector3 NowPosition;
@@ -21,9 +22,11 @@ public class SlimeController : MonoBehaviour
     private Mesh mesh;
     private int jumpIndex;
     private int squashIndex;
+    private bool squatInput = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        anim=GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         PresentPosition = new Vector3(0f, 3f, 0f);
         PresentScale = new Vector3(20f, 20f, 0f);
@@ -34,56 +37,73 @@ public class SlimeController : MonoBehaviour
         mesh = smr.sharedMesh;
 
         // 名前一覧確認
-        for (int i = 0; i < mesh.blendShapeCount; i++)
-            Debug.Log("Blend " + i + ": " + mesh.GetBlendShapeName(i));
+        //for (int i = 0; i < mesh.blendShapeCount; i++)
+            //Debug.Log("Blend " + i + ": " + mesh.GetBlendShapeName(i));
 
-        jumpIndex = smr.sharedMesh.GetBlendShapeIndex("ジャンプ");
-        squashIndex = smr.sharedMesh.GetBlendShapeIndex("しゃがむ");
+        //jumpIndex = smr.sharedMesh.GetBlendShapeIndex("ジャンプ");
+        //squashIndex = smr.sharedMesh.GetBlendShapeIndex("しゃがむ");
 
-        Debug.Log(jumpIndex + "," + squashIndex);
+        //Debug.Log(jumpIndex + "," + squashIndex);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // ジャンプ
-        Debug.Log(isGrounded);
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+
+
+        /*AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0); // 0 = Base Layer
+
+        if (stateInfo.IsName("Slime_Idle"))
         {
+            Debug.Log("いまIdle中！");
+        }
+        else if (stateInfo.IsName("Slime_Jump"))
+        {
+            Debug.Log("いまJump中！");
+        }
+        else if (stateInfo.IsName("Slime_Squash"))
+        {
+            Debug.Log("いまSquash中！");
+        }*/
+        squatInput = Input.GetKey(KeyCode.DownArrow); // ↓を押してる間しゃがむ
+        anim.SetBool("isSquatting", squatInput);
+        Debug.Log(squatInput);
+        // ジャンプ
+        //Debug.Log(isGrounded);
+        if (Input.GetKey(KeyCode.UpArrow) && isGrounded)
+        {
+            anim.SetTrigger("Jump");
             rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-            smr.SetBlendShapeWeight(jumpIndex, 100f);
+
+            //smr.SetBlendShapeWeight(jumpIndex, 100f);
             isGrounded = false;
             Debug.Log("Jump");
         }
-        if (Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            smr.SetBlendShapeWeight(jumpIndex, 0f);
-            Debug.Log("UnJump");
-        }
+
 
         // ジャンプ
         if (Input.GetKeyDown(KeyCode.DownArrow) && isGrounded)
         {
-            smr.SetBlendShapeWeight(squashIndex, 100f);
+            anim.SetTrigger("Squash");
             Debug.Log("Down");
         }
         if (Input.GetKeyUp(KeyCode.DownArrow))
         {
-            smr.SetBlendShapeWeight(squashIndex, 0f);
-            Debug.Log("UnDown");
+            squatInput = false;
         }/*
-        // 右ジャンプ
-        if (Input.GetKeyDown(KeyCode.RightArrow) && isGrounded)
-        {
-            rb.AddForce(new Vector3(sideForce, jumpPower, 0f), ForceMode.Impulse);
-            isGrounded = false;
-        }
-        // 左ジャンプ
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && isGrounded)
-        {
-            rb.AddForce(new Vector3(-sideForce, jumpPower, 0f), ForceMode.Impulse);
-            isGrounded = false;
-        }*/
+      
+         // 右ジャンプ
+         if (Input.GetKeyDown(KeyCode.RightArrow) && isGrounded)
+         {
+             rb.AddForce(new Vector3(sideForce, jumpPower, 0f), ForceMode.Impulse);
+             isGrounded = false;
+         }
+         // 左ジャンプ
+         if (Input.GetKeyDown(KeyCode.LeftArrow) && isGrounded)
+         {
+             rb.AddForce(new Vector3(-sideForce, jumpPower, 0f), ForceMode.Impulse);
+             isGrounded = false;
+         }*/
         // しゃがみ（押してる間ずっと）
         /*if (Input.GetKeyDown(KeyCode.DownArrow))
         {
